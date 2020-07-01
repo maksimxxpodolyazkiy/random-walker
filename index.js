73abc722522f24883353d48882c7b8dc3d78c4b4
maksimxxpodolyazkiy <53081588+maksimxxpodolyazkiy@users.noreply.github.com>
2020-07-01 17:59:34 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+class Walker {
+
+}
+
+function setup() {
+    createCanvas(1520, 700);
+}
+  
+function draw() {
+    background(25);
+    rect(620, 250, 200, 200);
+    rect(625, 255, 190, 190)
+}
